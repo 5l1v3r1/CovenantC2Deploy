e3f2040a365d31175d69f75213b9f2c0f35fe7ab
Daniel Card <dan.card@xservus.com>
2020-01-28 12:16:40 +0000

    Update covenant_docker_all.sh

--- covenant_docker_all.sh
+++ covenant_docker_all.sh
@@ -25,4 +25,5 @@ cd /pentest
 git clone --recurse-submodules https://github.com/cobbr/Covenant
 cd Covenant/Covenant
 docker build -t covenant .
-docker run -it -p 7443:7443 -p 80:80 -p 443:443 --name covenant -v /pentest/Covenant/Covenant/Data>:/app/Data covenant
+#run docker image
+docker run -it -p 7443:7443 -p 80:80 -p 443:443 --name covenant -v /pentest/Covenant/Covenant/Data covenant
