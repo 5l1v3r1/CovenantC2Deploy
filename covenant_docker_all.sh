f26e708f0274409379bb8a861b7ec67333063a7d
Daniel Card <dan.card@xservus.com>
2020-01-28 12:12:11 +0000

    Create covenant_docker_all.sh

---
+++ covenant_docker_all.sh
@@ -0,0 +1,28 @@
+# install docker and run covenant on Kali 2019.4
+# mRr3b00t
+# version 0.1
+
+#add docker to source lists
+#add docker gpg key
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+
+#install docker
+apt-get install docker-ce -y
+
+#start docker using systemctl
+systemctl start docker
+
+#set docker to start on boot using systemctl
+systemctl enable docker
+
+mkdir /pentest
+cd /pentest
+git clone --recurse-submodules https://github.com/cobbr/Covenant
+cd Covenant/Covenant
+docker build -t covenant .
+docker run -it -p 7443:7443 -p 80:80 -p 443:443 --name covenant -v /pentest/Covenant/Covenant/Data>:/app/Data covenant
