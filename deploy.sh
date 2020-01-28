12498f8351701502f79f3d36a22017615adab0f1
Daniel Card <dan.card@xservus.com>
2020-01-28 10:56:48 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# Covenant C2 on KALI (2019.4)
+# version 0.1 mRr3b00t
+# 28-01-2019
+# www.pwndefend.com
+# 'hack for good!'
+
+
+wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+
+apt-get install dotnet-sdk-2.2
+
+mkdir /pentest
+cd /pentest
+git clone --recurse-submodules https://github.com/cobbr/Covenant
+cd Covenant/Covenant
+dotnet build
+dotnet run
